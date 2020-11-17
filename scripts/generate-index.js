bdd1a640db8ffb795728dcd1439f2685193dc4d8
Francisco Pérez Sampayo <fpsampayo@gmail.com>
2020-11-17 15:33:15 +0100

    1.0.0 (#7)

--- scripts/generate-index.js
+++ scripts/generate-index.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const Mustache = require('mustache');
+const {version} = require('../package.json');
 
 const template = fs.readFileSync("index-template.html").toString();
 
 const data = {
-	"svg": fs.readdirSync('../docs/svg'),
-	"png64": fs.readdirSync('../docs/png/64'),
+    "version": 'v' + version,
+	"svg": fs.readdirSync('../static/svg'),
+	"png64": fs.readdirSync('../static/png/64'),
 };
 
 const output = Mustache.render(template, data);
 
-fs.writeFileSync('../docs/index.html', output);
+fs.writeFileSync('../static/index.html', output);
--- scripts/svg2png.js
+++ scripts/svg2png.js
@@ -2,15 +2,21 @@
 const fs = require('fs');
 const svg2png = require("svg2png");
 
-fs.readdirSync('../docs/svg').map(convert);
+const outputDir = '../static/png/64/';
+
+if (!fs.existsSync(outputDir)){
+    fs.mkdirSync(outputDir, {recursive: true});
+}
+
+fs.readdirSync('../svg').map(convert);
 
 function convert(file) {
 	let targetName = file.split(".")
 	targetName.pop();
 	targetName = targetName.join(".") + ".png";
 	console.log("Converting " + file + " to " + targetName);
-	const source = fs.readFileSync('../docs/svg/'+file);
+	const source = fs.readFileSync('../svg/'+file);
 	const target = svg2png.sync(source);
 	
-	fs.writeFileSync('../docs/png/64/'+targetName, target);
+	fs.writeFileSync(outputDir + targetName, target);
 }
