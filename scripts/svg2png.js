258883e44a7e9532f5de06c996ba9930b2ecb05b
sergioedo <sergio.edo@gmail.com>
2022-04-29 07:14:57 +0200

    adjust viewbox param on svg files

--- scripts/svg2png.js
+++ scripts/svg2png.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
+const path = require('path');
 const fs = require('fs');
-const svg2png = require("svg2png");
+const svg2png = require('svg2png');
 
-const outputDir = '../static/png/64/';
+const arguments = process.argv;
+
+// Too much arguments, show help & exit
+if (arguments.length > 3) {
+	console.log(`Usage: ${path.basename(__filename)} [image_height_in_pixels]`);
+	process.exit(1);
+}
+
+const imageHeight = parseInt(arguments[arguments.length - 1]);
+const config = imageHeight > 0 ? { height: imageHeight } : undefined;
+
+const outputDir = `../static/png/${config ? config.height : 'originalSize'}/`;
+console.log(`Outputting to ${outputDir}`);
 
 if (!fs.existsSync(outputDir)){
     fs.mkdirSync(outputDir, {recursive: true});
@@ -16,7 +29,7 @@ function convert(file) {
 	targetName = targetName.join(".") + ".png";
 	console.log("Converting " + file + " to " + targetName);
 	const source = fs.readFileSync('../svg/'+file);
-	const target = svg2png.sync(source);
+	const target = svg2png.sync(source, config);
 	
 	fs.writeFileSync(outputDir + targetName, target);
 }
